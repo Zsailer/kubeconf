36a7ee4e9dfff50e11329daa9dde63d8e2e5d466
Zsailer <zachsailer@gmail.com>
2018-10-18 17:00:05 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+import io
+import os
 from setuptools import setup, find_packages
 
+# Use README for long description
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
 
 setup(
@@ -8,4 +14,21 @@ setup(
     description='Simple tool for managing your kubeconfig.',
     author='Zach Sailer',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author_email='zachsailer@gmail.com',
+    url='https://github.com/Zsailer/kubeconf',
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
